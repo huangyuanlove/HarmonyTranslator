28eaa1c5be1931ac80aa3ac27f877b36d2726852
huangyuan <huangyuan@huangyuanlove.com>
2023-08-13 10:48:34 +0800

    设置appkey和appid

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -2,9 +2,11 @@ import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 
+
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+
   }
 
   onDestroy() {
