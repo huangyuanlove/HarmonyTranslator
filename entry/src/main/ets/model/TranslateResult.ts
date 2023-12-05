e4924949117ee8f7f132e5b20f6e06622a265044
huangyuan <huangyuan@chunyu.me>
2023-12-05 19:08:25 +0800

    英语翻译到汉语

--- entry/src/main/ets/model/TranslateResult.ts
+++ entry/src/main/ets/model/TranslateResult.ts
@@ -1,4 +1,5 @@
 import List from '@ohos.util.List'
+
 export class TranslationResult {
   query: string
   translations: TranslationGroup[]
@@ -80,9 +81,6 @@ export class BaiduAIGeneralTranslationResult {
 }
 
 
-
-
-
 ////// 百度机器翻译词典版
 
 
@@ -95,7 +93,7 @@ export class MeansInSimple {
 export class BaiduAIDictSimpleMeans {
   from: string
   word_symbol: string
-  word_name:string
+  word_name: string
   means: MeansInSimple[]
 
   static fromJSON(json: Object): BaiduAIDictSimpleMeans {
@@ -137,50 +135,53 @@ export class BaiduAIDictSimpleMeans {
       }
     }
 
-
     return tmp
   }
 }
 
-export class ZDict{
-  detail:ZDictDetail
 
-  static fromJSON(json:Object):ZDict{
-    let tmp:ZDict = new ZDict()
+export class ZDict {
+  detail: ZDictDetail
+  simple: ZDictDetail
 
-    let detailJSON:JSON = json['detail']
-    if(detailJSON){
+  static fromJSON(json: Object): ZDict {
+    let tmp: ZDict = new ZDict()
+
+    let detailJSON: JSON = json['detail']
+    if (detailJSON) {
       tmp.detail = ZDictDetail.fromJSON(detailJSON)
+    }
+    let simpleJSON: JSON = json['simple']
+    if (simpleJSON) {
+      tmp.simple = ZDictDetail.fromJSON(simpleJSON)
     }
 
     return tmp;
   }
-
 }
 
-export class ZDictDetail{
+export class ZDictDetail {
+  idiom: ZDictDetailIdiom
+  means: ZDictDetailMeans[]
 
-  idiom:ZDictDetailIdiom
-  means:ZDictDetailMeans[]
+  static fromJSON(json: Object): ZDictDetail {
+    let tmp: ZDictDetail = new ZDictDetail()
 
-  static fromJSON(json:Object):ZDictDetail{
-    let tmp :ZDictDetail = new ZDictDetail()
-
-    let idiomJSON:JSON = json['chenyu']
-    if(idiomJSON){
+    let idiomJSON: JSON = json['chenyu']
+    if (idiomJSON) {
       tmp.idiom = ZDictDetailIdiom.fromJSON(idiomJSON)
     }
 
-    let meansJSON:JSON[] = json['means']
-    if(meansJSON && meansJSON.length >0){
-      let exp:JSON[] = meansJSON[0]['exp']
-      if(exp && exp.length > 0){
-        let des:JSON[] = exp[0]['des']
-        if(des && des.length >0){
+    let meansJSON: JSON[] = json['means']
+    if (meansJSON && meansJSON.length > 0) {
+      let exp: JSON[] = meansJSON[0]['exp']
+      if (exp && exp.length > 0) {
+        let des: JSON[] = exp[0]['des']
+        if (des && des.length > 0) {
           tmp.means = []
-          des.forEach((value)=>{
-            let zDictDetailMeans:ZDictDetailMeans = ZDictDetailMeans.fromJSON(value)
-            if(zDictDetailMeans){
+          des.forEach((value) => {
+            let zDictDetailMeans: ZDictDetailMeans = ZDictDetailMeans.fromJSON(value)
+            if (zDictDetailMeans) {
               tmp.means.push(zDictDetailMeans)
             }
           })
@@ -190,24 +191,23 @@ export class ZDictDetail{
 
     return tmp;
   }
-
 }
 
 
-export class ZDictDetailMeans{
-  main:string
-  sub:List<string>
+export class ZDictDetailMeans {
+  main: string
+  sub: List<string>
 
-  static fromJSON(json:Object):ZDictDetailMeans{
+  static fromJSON(json: Object): ZDictDetailMeans {
     let tmp = new ZDictDetailMeans()
     tmp.main = json['main']
 
-    let subJSON:JSON[] =json['sub']
-    if(subJSON && subJSON.length > 0){
+    let subJSON: JSON[] = json['sub']
+    if (subJSON && subJSON.length > 0) {
       tmp.sub = new List()
 
-      subJSON.forEach((value)=>{
-        tmp.sub.add( JSON.stringify(value))
+      subJSON.forEach((value) => {
+        tmp.sub.add(JSON.stringify(value))
       })
 
     }
@@ -215,22 +215,20 @@ export class ZDictDetailMeans{
 
     return tmp
   }
-
 }
 
 
-export class ZDictDetailIdiom{
+export class ZDictDetailIdiom {
+  antonym: string
+  example: string
+  explain: string
+  from: string
+  grammer: string
+  pinyin: string
+  synonyms: string
 
-  antonym:string
-  example:string
-  explain:string
-  from:string
-  grammer:string
-  pinyin:string
-  synonyms:string
-
-  static fromJSON(json:Object):ZDictDetailIdiom{
-    let tmp : ZDictDetailIdiom = new ZDictDetailIdiom()
+  static fromJSON(json: Object): ZDictDetailIdiom {
+    let tmp: ZDictDetailIdiom = new ZDictDetailIdiom()
     tmp.antonym = json['antonym']
     tmp.example = json['example']
     tmp.explain = json['explain']
@@ -240,53 +238,169 @@ export class ZDictDetailIdiom{
     tmp.synonyms = json['synonyms']
     return tmp;
   }
-
 }
 
 
+export class PartsForEngInSymbols {
+  part: string
+  means: string[]
 
+  static fromJSON(json: Object): PartsForEngInSymbols {
+    let tmp: PartsForEngInSymbols = new PartsForEngInSymbols()
+    tmp.part = json['part']
+    let meansJSON: JSON[] = json['means']
+    if (meansJSON && meansJSON.length > 0) {
+      tmp.means = []
+      meansJSON.forEach((value) => {
+        tmp.means.push(JSON.stringify(value))
+      })
+    }
+
+    return tmp;
+  }
+}
+
+export class SymbolsForEng {
+  ph_en: string
+  ph_am: string
+  parts: PartsForEngInSymbols[]
+
+  static fromJSON(json: Object): SymbolsForEng {
+    let tmp: SymbolsForEng = new SymbolsForEng()
+
+    tmp.ph_en = json['ph_en']
+    tmp.ph_am = json['ph_am']
+    let partsJSON: JSON[] = json['parts']
+    if (partsJSON && partsJSON.length > 0) {
+      tmp.parts = []
+      partsJSON.forEach((value) => {
+        tmp.parts.push(PartsForEngInSymbols.fromJSON(value))
+      })
+    }
+
+    return tmp;
+  }
+}
+
+export class ExchangeForEngInSimpleMeans {
+  word_third: string[]
+  word_ing: string[]
+  word_done: string[]
+  word_pl: string[]
+  word_past: string[]
+
+  static fromJSON(json: Object): ExchangeForEngInSimpleMeans {
+    let tmp: ExchangeForEngInSimpleMeans = new ExchangeForEngInSimpleMeans()
+
+    let thirdJSON: string[] = json['word_third']
+    let ingJSON: string[] = json['word_ing']
+    let doneJSON: string[] = json['word_done']
+    let plJSON: string[] = json['word_pl']
+    let pastJSON: string[] = json['word_past']
+    tmp.word_third = thirdJSON
+    tmp.word_ing = ingJSON
+    tmp.word_done = doneJSON
+    tmp.word_pl = plJSON
+    tmp.word_past = pastJSON
+
+
+    return tmp;
+  }
+}
+
+
+export class BaiduAIDictSimpleMeansForEng {
+  symbols: SymbolsForEng
+  tags: string[]
+  exchange: ExchangeForEngInSimpleMeans
+  from: string
+  word_name:string
+
+  static fromJSON(json: Object): BaiduAIDictSimpleMeansForEng {
+    let tmp: BaiduAIDictSimpleMeansForEng = new BaiduAIDictSimpleMeansForEng()
+    let symbolsJSON: JSON[] = json['symbols']
+    if (symbolsJSON) {
+      tmp.symbols = SymbolsForEng.fromJSON(symbolsJSON)
+    }
+
+    let tagJSON: JSON = json['tags']
+    if (tagJSON) {
+      let coreJSON: string[] = tagJSON['core']
+      if (coreJSON && coreJSON.length > 0) {
+        tmp.tags = coreJSON
+      }
+
+      let otherJSON: string[] = tagJSON['other']
+      if (tagJSON && otherJSON.length > 0) {
+        if (tmp.tags) {
+          tmp.tags.concat(otherJSON)
+        }
+      }
+    }
+
+    let exchangeJSON: JSON = json['exchange']
+    if (exchangeJSON) {
+      tmp.exchange = ExchangeForEngInSimpleMeans.fromJSON(exchangeJSON)
+    }
+
+    tmp.from = json['from']
+    tmp.word_name = json['word_name']
+
+
+    return tmp;
+  }
+}
 
 export class WordResult {
+  lang: String
   simpleMeans: BaiduAIDictSimpleMeans
-  zDict:ZDict
-  static fromJSON(json: Object): WordResult {
-    let tmp: WordResult = new WordResult();
+  simpleMeansForEng: BaiduAIDictSimpleMeansForEng
+  zDict: ZDict
 
-    let worResult: JSON = JSON.parse(json as string)['word_result']
-    tmp.simpleMeans = BaiduAIDictSimpleMeans.fromJSON(worResult['simple_means'])
+
+  static fromJSON(jsonStr: string): WordResult {
+    let tmp: WordResult = new WordResult();
+    let json:JSON = JSON.parse(jsonStr)
+    let worResult: JSON =  json['word_result']
+    if (worResult) {
+      tmp.lang = json['lang']
+    }
+    if (tmp.lang && tmp.lang == '1') {
+      tmp.simpleMeansForEng = BaiduAIDictSimpleMeansForEng.fromJSON(worResult['simple_means'])
+    } else {
+      tmp.simpleMeans = BaiduAIDictSimpleMeans.fromJSON(worResult['simple_means'])
+    }
 
     let zDictJSON = worResult['zdict']
-    if(zDictJSON){
+    if (zDictJSON) {
       tmp.zDict = ZDict.fromJSON(zDictJSON)
     }
     return tmp
   }
 }
 
-export class BaiduAIDictResult{
-  dst:string
+export class BaiduAIDictResult {
+  dst: string
   dst_tts: string
   src: string
   src_tts: string
   dict: WordResult
 
-  static fromJSON(json:Object):BaiduAIDictResult{
+  static fromJSON(json: Object): BaiduAIDictResult {
     let tmp = new BaiduAIDictResult()
-    if(json){
+    if (json) {
       tmp.dst = json['dst']
       tmp.dst_tts = json['dst_tts']
       tmp.src = json['src']
       tmp.src_tts = json['src_tts']
-      let dictJSON:JSON = json['dict']
-      if(dictJSON){
+      let dictJSON: string = json['dict']
+      if (dictJSON) {
         tmp.dict = WordResult.fromJSON(dictJSON)
       }
 
     }
     return tmp;
   }
-
-
 }
 
 
